9528029cb3b575e5bd96825760ca1a6afbac2948
devcon <devcon@admantium.com>
2018-12-31 11:34:09 +0100

    feature(chrome-node): set default '$HUB_PORT=4444' and '$HUB_HOST=127.0.0.1'

--- chrome-node/build/start.sh
+++ chrome-node/build/start.sh
@@ -1,6 +1,8 @@
-#!/bin/sh 
+#!/bin/sh
+export HUB_PORT="${HUB_PORT:-4442}"
+export HUB_HOST="${HUB_HOST:-127.0.0.1}"
 java -Dwebdriver.chrome.driver="/usr/bin/chromedriver" \
-   -Dwebdriver.chrome.whitelistedIps=$HUB_HOST  \
+   -Dwebdriver.chrome.whitelistedIps=${HUB_HOST}  \
    -Dwebdriver.enable.native.events=1 \
-   -jar /etc/chrome-node/lib/selenium.jar -role node -hub http://$HUB_HOST:$HUB_PORT -nodeConfig /etc/chrome-node/conf/node.conf.json &
+   -jar /etc/chrome-node/lib/selenium.jar -role node -hub http://${HUB_HOST}:${HUB_PORT} -nodeConfig /etc/chrome-node/conf/node.conf.json &
 wait $!
